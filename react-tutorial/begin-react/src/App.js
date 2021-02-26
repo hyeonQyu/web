ab4f0d9729e5ae90bf7a35dc1b47ec7d3d742beb
hyeonQyu <dhk0561@naver.com>
2021-02-26 12:54:17 +0900

    태그는 꼭 닫혀있어야 함, 값을 보여줄 때는 {}로 감싼다, class attr은 className으로 표시

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import Hello from './Hello';
 import './App.css';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* 주석은 이렇게 */}
+      <Hello />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
