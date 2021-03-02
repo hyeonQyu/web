eee94542942e17bc78875d8f75b56597c5fd31ff
hyeonQyu <dhk0561@naver.com>
2021-03-02 16:46:26 +0900

    커스텀 hooks인 useInput에서 사용한 useState를 useReducer로 변경

--- react-tutorial/begin-react/src/useInputs.js
+++ react-tutorial/begin-react/src/useInputs.js
@@ -1,19 +1,47 @@
-import {useState, useCallback} from 'react';
+//import {useState, useCallback} from 'react';
+import {useReducer, useCallback} from 'react';
+
+function reducer(state, action){
+    switch(action.type){
+        case 'CHANGE_INPUTS':
+            return {
+                ...state,
+                [action.name]: action.value,
+            };
+        case 'RESET_INPUTS':
+            // return Object.keys(state).reduce((acc, current) => {
+            //     acc[current] = '';
+            //     return acc;
+            // }, {});
+            const obj = {};
+            const arr = Object.keys(state);
+            for(const name of arr){
+                obj[name] = '';
+            }
+            return obj;
+        default:
+            return state;
+    }
+}
 
 function useInputs(initialForm){
-    const [form, setForm] = useState(initialForm);
+    // const [form, setForm] = useState(initialForm);
+    const [form, dispatch] = useReducer(reducer, initialForm);
     
     const onChange = useCallback(e => {
         const {name, value} = e.target;
-        setForm(form => {
-            return {
-                ...form,
-                [name]: value,
-            }
+        dispatch({
+            type: 'CHANGE_INPUTS',
+            name,
+            value,
         });
     }, []);
 
-    const reset = useCallback(() => setForm(initialForm), [initialForm]);
+    const reset = useCallback(() => {
+        dispatch({
+            type: 'RESET_INPUTS',
+        });
+    });
 
     return [form, onChange, reset];
 }
