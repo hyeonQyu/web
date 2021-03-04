f2a176f18927e9eedeb5615498030dea6decf778
hyeonQyu <dhk0561@naver.com>
2021-03-04 14:40:04 +0900

    ESLint 설정

--- react-tutorial/useful-tools/src/App.js
+++ react-tutorial/useful-tools/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-const asd = 'sadf';
-
 function App() {
+    const [value, setValue] = useState(0);
+    useEffect(() => {
+        console.log(value);
+    }, [value]);
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div>
+            <div>adsfklj</div>
         </div>
     );
 }
