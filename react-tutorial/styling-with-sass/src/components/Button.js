9cafde19a85b681fe47aa8bc92cd4d78adb84519
hyeonQyu <dhk0561@naver.com>
2021-03-05 10:43:10 +0900

    버튼 색상 설정과 @mixin과 @include를 통한 sass 코드 중복 제거

--- react-tutorial/styling-with-sass/src/components/Button.js
+++ react-tutorial/styling-with-sass/src/components/Button.js
@@ -2,14 +2,19 @@ import React from 'react';
 import classNames from 'classnames';
 import './Button.scss';
 
-// large, medium, small
-function Button({ children, size }) {
-    console.log('ads');
-    return <button className={classNames('Button', size)}>{children}</button>;
+// size: large, medium, small
+// color: blue, pink, gray
+function Button({ children, size, color }) {
+    return (
+        <button className={classNames('Button', size, color)}>
+            {children}
+        </button>
+    );
 }
 
 Button.defaultProps = {
     size: 'medium',
+    color: 'blue',
 };
 
 export default Button;
