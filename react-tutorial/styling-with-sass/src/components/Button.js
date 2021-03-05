fa24624f07481dba6e748520c415cc986bc551eb
hyeonQyu <dhk0561@naver.com>
2021-03-05 10:32:30 +0900

    버튼 컴포넌트 생성 및 여러가지 크기

---
+++ react-tutorial/styling-with-sass/src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classNames from 'classnames';
+import './Button.scss';
+
+// large, medium, small
+function Button({ children, size }) {
+    console.log('ads');
+    return <button className={classNames('Button', size)}>{children}</button>;
+}
+
+Button.defaultProps = {
+    size: 'medium',
+};
+
+export default Button;
