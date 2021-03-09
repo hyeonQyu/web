0d95346a9612f9c7b48439e7b83ebc9a04074ce9
hyeonQyu <dhk0561@naver.com>
2021-03-09 09:50:53 +0900

    할 일 추가

--- react-tutorial/mashup-todolist/src/components/TodoCreate.js
+++ react-tutorial/mashup-todolist/src/components/TodoCreate.js
@@ -1,33 +1,47 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './scss/TodoCreate.scss';
 import { MdAdd } from 'react-icons/md';
 import classNames from 'classnames';
+import { useTodoDispatch, useTodoNextId } from '../TodoContext';
 
 function TodoCreate() {
     const [open, setOpen] = useState('close');
+    const [value, setValue] = useState('');
+
+    const dispatch = useTodoDispatch();
+    const nextId = useTodoNextId();
+
     const onToggle = () => setOpen(open === 'open' ? 'close' : 'open');
+    const onChange = (e) => setValue(e.target.value);
+    const onSubmit = (e) => {
+        e.preventDefault();
+        dispatch({
+            type: 'CREATE',
+            todo: {
+                id: nextId.current,
+                text: value,
+                done: false,
+            },
+        });
+        setValue('');
+        setOpen('close');
+        nextId.current++;
+    };
 
     return (
         <>
             {open === 'open' && (
-                <div className="insert-form-positioner">
+                <form className="insert-form-positioner" onSubmit={onSubmit}>
                     <div className="insert-form">
-                        <input
-                            placeholder="할 일을 입력 후 Enter를 누르세요"
-                            className="input"
-                            autoFocus
-                        ></input>
+                        <input placeholder="할 일을 입력 후 Enter를 누르세요" className="input" autoFocus onChange={onChange} value={value}></input>
                     </div>
-                </div>
+                </form>
             )}
-            <div
-                className={classNames('circle-button', open)}
-                onClick={onToggle}
-            >
+            <div className={classNames('circle-button', open)} onClick={onToggle}>
                 <MdAdd />
             </div>
         </>
     );
 }
 
-export default TodoCreate;
+export default React.memo(TodoCreate);
