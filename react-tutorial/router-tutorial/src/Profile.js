2a46985f66777de0c4216fc30ce2d0bf56f4b0c7
hyeonQyu <dhk0561@naver.com>
2021-03-09 16:05:26 +0900

    파라미터와 쿼리를 사용한 라우팅

---
+++ react-tutorial/router-tutorial/src/Profile.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const profileData = {
+    hyeonQyu: {
+        name: '김현규',
+        description: 'I want backend',
+    },
+    homer: {
+        name: '호머심슨',
+        description: '아빠심슨',
+    },
+};
+function Profile({ match }) {
+    const { username } = match.params;
+    const profile = profileData[username];
+
+    if (!profile) {
+        return <div>존재하지 않는 사용자입니다.</div>;
+    }
+
+    return (
+        <div>
+            <h3>
+                {username} ({profile.name})
+            </h3>
+            <p>{profile.description}</p>
+        </div>
+    );
+}
+
+export default Profile;
