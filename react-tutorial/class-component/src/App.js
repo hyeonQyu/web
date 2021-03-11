dd359666621019f904fa11bb5a2fb75840c3ed6b
hyeonQyu <dhk0561@naver.com>
2021-03-11 15:04:15 +0900

    북 리스트 예제

--- react-tutorial/class-component/src/App.js
+++ react-tutorial/class-component/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import BookList from './components/BookList';
+import Books from './static_data/Books';
 
 class App extends Component {
     render() {
-        return <BookList />;
+        return <BookList books={Books} />;
     }
 }
 
