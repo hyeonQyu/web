b81f6368d488dccb61700c127348c3616415ebf8
hyeonQyu <dhk0561@naver.com>
2021-04-01 23:56:42 +0900

    CounterStore 전역 설정

--- react-tutorial/counter-mobx/src/component/CounterComponent.js
+++ react-tutorial/counter-mobx/src/component/CounterComponent.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import { Button, Box } from '@material-ui/core';
+import { inject, observer } from 'mobx-react';
 
+@inject('counterStore')
+@observer
 class CounterComponent extends Component {
+    render() {
+        const { counterStore } = this.props;
 
-  render(){
-    return(
-      <div>
-        <Button variant='contained' color='primary' size='large'> - </Button>        
-        
-        <Box component='span' m={5}> 0 </Box>
-        
-        <Button variant='contained' color='primary' size='large'> + </Button>
-      </div>
-    )
-  }
+        return (
+            <div>
+                <Button variant="contained" color="primary" size="large">
+                    -
+                </Button>
+
+                <Box component="span" m={5}>
+                    {counterStore._count}
+                </Box>
+
+                <Button variant="contained" color="primary" size="large">
+                    +
+                </Button>
+            </div>
+        );
+    }
 }
 
 export default CounterComponent;
--- react-tutorial/counter-mobx/src/index.js
+++ react-tutorial/counter-mobx/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'mobx-react';
+import CounterStore from './store/CounterStore';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    // App에 있는 모든 컴포넌트들이 CounterStore를 전역적으로 사용할 수 있음
+    <Provider counterStore={CounterStore}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
 );
 
 reportWebVitals();
