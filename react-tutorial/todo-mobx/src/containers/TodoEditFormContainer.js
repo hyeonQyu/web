60dd43a3ef4c43bd83d6d166d94503dbe4dc295c
hyeonQyu <dhk0561@naver.com>
2021-04-02 23:24:06 +0900

    Store > Container > View로 State 전달 및 연결 Container에서 Store를 주입받고 Store 내의 State를 View로 전달

--- react-tutorial/todo-mobx/src/containers/TodoEditFormContainer.js
+++ react-tutorial/todo-mobx/src/containers/TodoEditFormContainer.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import TodoEditFormView from '../views/TodoEdotFormView';
+import TodoEditFormView from '../views/TodoEditFormView';
+import { inject, observer } from 'mobx-react';
 
-
+@inject('todoStore')
+@observer
 class TodoEditFormContainer extends Component {
-  render(){
-    return(
-      <TodoEditFormView />
-    )
-  }
+    render() {
+        const { todoStore } = this.props;
+        return <TodoEditFormView todo={todoStore.todo} />;
+    }
 }
 
 export default TodoEditFormContainer;
--- react-tutorial/todo-mobx/src/index.js
+++ react-tutorial/todo-mobx/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'mobx-react';
+import TodoStore from './stores/TodoStore';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider todoStore={TodoStore}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
 );
 
 reportWebVitals();
