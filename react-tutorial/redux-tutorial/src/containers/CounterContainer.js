af187f44647e9cfd24dd2a7f0900080cf4c3d6a9
hyeonQyu <dhk0561@naver.com>
2021-07-04 18:58:44 +0900

    프리젠테이셔널 컴포넌트와 컨테이너 컴포넌트를 분리하여 Counter 구현

---
+++ react-tutorial/redux-tutorial/src/containers/CounterContainer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Counter from '../components/Counter';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrease, increase, setDiff } from '../modules/counter';
+
+function CounterContainer() {
+    // useSelector는 상태를 조회하는 hook
+    const { number, diff } = useSelector((state) => ({
+        number: state.counter.number,
+        diff: state.counter.diff,
+    }));
+
+    // action을 dispatch하는 hook
+    const dispatch = useDispatch();
+    const onIncrease = () => dispatch(increase());
+    const onDecrease = () => dispatch(decrease());
+    const onSetDiff = (diff) => dispatch(setDiff(diff));
+
+    return <Counter number={number} diff={diff} onIncrease={onIncrease} onDecrease={onDecrease} onSetDiff={onSetDiff} />;
+}
+
+export default CounterContainer;
