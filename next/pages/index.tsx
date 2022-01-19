ed04ecd1bb389b39eb5c6d3ccd1f1f6630a8b2a0
Hyeongyu Kim <dhk0561@naver.com>
2022-01-19 22:38:07 +0900

    next.js styles JSX 사용

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,10 +1,17 @@
 import NavBar from '../components/NavBar';
 
 export default function Home() {
+    const size = 50;
+
     return (
         <>
             <NavBar />
             <h1>Hello</h1>
+            <style jsx>{`
+                h1 {
+                    font-size: ${size}px;
+                }
+            `}</style>
         </>
     );
 }
