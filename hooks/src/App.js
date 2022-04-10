9a8bce897cf837db3ee65e998512a440d696ef42
Hyeongyu Kim <dhk0561@naver.com>
2022-04-10 18:38:22 +0900

    useTitle hook

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import useTabs from './hooks/useTabs';
+import useInput from './hooks/useInput';
+import useTitle from './hooks/useTitle';
 
 const content = [
     {
@@ -13,11 +15,17 @@ const content = [
 ];
 
 function App() {
+    const maxLength = (value) => value.length <= 10;
+    const name = useInput('Mr.', maxLength);
     const { currentItem, changeItem } = useTabs(0, content);
+    const setTitle = useTitle('Loading..');
+    setTimeout(() => setTitle('Home'), 5000);
 
     return (
         <div className="App">
             <h1>hello</h1>
+            <input placeholder={'name'} {...name} />
+            <br />
             {content.map((section, index) => (
                 <button onClick={() => changeItem(index)}>{section.tab}</button>
             ))}
---
+++ hooks/src/hooks/useTitle.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+const useTitle = (initialTitle) => {
+    const [title, setTitle] = useState(initialTitle);
+
+    useEffect(() => {
+        const htmlTitle = document.querySelector('title');
+        htmlTitle.innerText = title;
+    }, [title]);
+
+    return setTitle;
+};
+
+export default useTitle;
