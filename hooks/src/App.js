5411c8467cbff41033753d3a5c07cf58a206d158
Hyeongyu Kim <dhk0561@naver.com>
2022-04-10 18:29:09 +0900

    useTabs hook

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
-import useInput from './hooks/useInput';
+import useTabs from './hooks/useTabs';
+
+const content = [
+    {
+        tab: 'section 1',
+        content: 'section 1 content',
+    },
+    {
+        tab: 'section 2',
+        content: 'section 2 content',
+    },
+];
 
 function App() {
-    const maxLength = (value) => value.length <= 10;
-    const name = useInput('Mr.', maxLength);
+    const { currentItem, changeItem } = useTabs(0, content);
 
     return (
         <div className="App">
             <h1>hello</h1>
-            <input placeholder={'name'} {...name} />
+            {content.map((section, index) => (
+                <button onClick={() => changeItem(index)}>{section.tab}</button>
+            ))}
+            <div>{currentItem.content}</div>
         </div>
     );
 }
