6f812cca3d6411a3a9e124b4f1e4f2d39f35b250
hyeonQyu <dhk0561@naver.com>
2022-11-02 17:35:23 +0900

    feat: 유튜브 비디오 조회 API 추가

---
+++ 2022/youtube-clone/src/defines/youtube.ts
@@ -0,0 +1,26 @@
+export interface YoutubeVideo {
+  id: string;
+  etag: string;
+  kind: string;
+  snippet: {
+    publishedAt: string;
+    title: string;
+    description: string;
+    thumbnails: {
+      default: Thumbnail;
+      medium: Thumbnail;
+      high: Thumbnail;
+      standard: Thumbnail;
+    };
+    channelId: string;
+    channelTitle: string;
+    tags: string[];
+    categoryId: number;
+  };
+}
+
+export interface Thumbnail {
+  url: string;
+  width: number;
+  height: number;
+}
---
+++ 2022/youtube-clone/src/pages/api/videos.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { YoutubeVideo } from '@defines/youtube';
+import axios from 'axios';
+
+export interface VideosReq {
+  pageToken?: string;
+  maxResults: number;
+}
+
+export interface VideosRes {
+  etag: string;
+  items: YoutubeVideo[];
+  kind: string;
+  nextPageToken: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<VideosRes>) {
+  const { data } = await axios.get<VideosRes>('https://www.googleapis.com/youtube/v3/videos', {
+    params: {
+      ...req.query,
+      part: 'snippet',
+      chart: 'mostPopular',
+      key: process.env.YOUTUBE_API_KEY,
+    },
+  });
+  res.status(200).json(data);
+}
