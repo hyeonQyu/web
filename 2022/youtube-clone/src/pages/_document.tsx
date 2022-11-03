14f192a21cbfcf10b96d7d8d41ce0b66fb04d001
Hyeongyu Kim <dhk0561@naver.com>
2022-11-03 23:31:56 +0900

    chore: Pretendard 폰트 적용

--- 2022/youtube-clone/src/pages/_document.tsx
+++ 2022/youtube-clone/src/pages/_document.tsx
@@ -1,10 +1,12 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css" />
+        </Head>
         <body>
           <div id={'portal'} />
           <Main />
