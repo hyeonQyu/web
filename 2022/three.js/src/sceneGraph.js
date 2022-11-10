76c465402d1afbe5d6944e2138096933ed3673fb
Hyeongyu Kim <dhk0561@naver.com>
2022-11-10 23:25:13 +0900

    sceneGraph.js 기본 코드 생성

---
+++ 2022/three.js/src/sceneGraph.js
@@ -0,0 +1,93 @@
+import * as THREE from 'three';
+
+class Basic {
+  _$container;
+  _renderer;
+  _scene;
+  _camera;
+  _cube;
+
+  constructor() {
+    this._$container = document.querySelector('#webgl-container');
+
+    this._setupRenderer();
+    this._setupScene();
+
+    this._setupCamera();
+    this._setupLight();
+    this._setupModel();
+
+    // resize 함수 내에서 다루는 this가 이벤트 객체가 아닌 App 객체를 가르키도록 함
+    window.onresize = this.resize.bind(this);
+    this.resize();
+
+    // frame에 render 메소드 호출
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  resize() {
+    const { width, height } = this._getContainerSize();
+
+    this._camera.aspect = width / height;
+    this._camera.updateProjectionMatrix();
+
+    this._renderer.setSize(width, height);
+  }
+
+  render(ms) {
+    this._renderer.render(this._scene, this._camera);
+    this.update(ms);
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  update(ms) {
+    const seconds = ms * 0.001;
+    this._cube.rotation.x = seconds;
+    this._cube.rotation.y = seconds;
+  }
+
+  _setupRenderer() {
+    this._renderer = new THREE.WebGLRenderer({ antialias: true });
+    this._renderer.setPixelRatio(window.devicePixelRatio);
+    this._$container.appendChild(this._renderer.domElement);
+  }
+
+  _setupScene() {
+    this._scene = new THREE.Scene();
+  }
+
+  _setupCamera() {
+    const { width, height } = this._getContainerSize();
+
+    this._camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
+    this._camera.position.z = 2;
+  }
+
+  _setupLight() {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+
+    this._scene.add(light);
+  }
+
+  _setupModel() {
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshPhongMaterial({ color: 0x44a88 });
+    this._cube = new THREE.Mesh(geometry, material);
+
+    this._scene.add(this._cube);
+  }
+
+  _getContainerSize() {
+    const width = this._$container.clientWidth;
+    const height = this._$container.clientHeight;
+
+    return { width, height };
+  }
+}
+
+window.onload = function () {
+  new Basic();
+};
