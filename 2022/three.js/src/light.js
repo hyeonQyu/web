1c247eb0c412b12bd969e5b27c8830c74d94914b
Hyeongyu Kim <dhk0561@naver.com>
2022-12-03 19:58:59 +0900

    태양과 같은 광원 DirectionalLight

--- 2022/three.js/src/light.js
+++ 2022/three.js/src/light.js
@@ -70,10 +70,12 @@ class Basic {
   }
 
   _setupLight() {
-    const light = new THREE.HemisphereLight(0xb0d8f5, 0xbb7a1c, 2);
+    this._light = new THREE.DirectionalLight(0xffffff, 2);
+    this._light.position.set(0, 5, 0);
+    this._light.target.position.set(0, 0, 0);
 
-    this._scene.add(light);
-    this._light = light;
+    this._scene.add(this._light.target);
+    this._scene.add(this._light);
   }
 
   _setupControls() {
