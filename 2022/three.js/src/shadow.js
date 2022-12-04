743c6562fc0b5ad7465e092f73ce5db374d15b92
Hyeongyu Kim <dhk0561@naver.com>
2022-12-04 21:33:04 +0900

    그림자 품질 향상 (기본값: 512)

--- 2022/three.js/src/shadow.js
+++ 2022/three.js/src/shadow.js
@@ -86,8 +86,11 @@ class Basic {
     this._light.position.set(0, 5, 0);
     this._light.target.position.set(0, 0, 0);
     this._light.castShadow = true;
+
     this._light.shadow.camera.top = this._light.shadow.camera.right = 6;
     this._light.shadow.camera.bottom = this._light.shadow.camera.left = -6;
+
+    this._light.shadow.mapSize.width = this._light.shadow.mapSize.height = 2048;
 
     this._scene.add(this._light);
     this._scene.add(this._light.target);
